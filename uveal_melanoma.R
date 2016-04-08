6d61f8496380ec15a08e215b1b7662ab0f06ed45
Jun Kang <jun@ip-172-31-1-179.ap-northeast-2.compute.internal>
2016-04-08 05:24:39 +0000

    With GDAC data

--- uveal_melanoma.R
+++ uveal_melanoma.R
@@ -4,9 +4,22 @@
 
 untar('gdac.broadinstitute.org_UVM.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2016012800.0.0.tar.gz', exdir = '.')
 
-clinical <- read.delim('./clinical_data_portal/nationwidechildrens.org_clinical_patient_uvm.txt')
+untar('gdac.broadinstitute.org_UVM-TP.Aggregate_AnalysisFeatures.Level_4.2015082100.0.0.tar.gz', exdir = '.')
 
-summary(clinical)
+data <- read.delim('./gdac.broadinstitute.org_UVM-TP.Aggregate_AnalysisFeatures.Level_4.2015082100.0.0/UVM-TP.transferedsamplefeatures.txt')
+
+data[1:5,1:5]
+data_gdac <- t(data[,-1])
+colnames(data_gdac) <- data[,1]
+rownames(data_gdac) <- colnames(data)[-1]
+data <- data.frame(data_gdac[-dim(data_gdac)[1],])
+
+data[, c(1:4, 15:250)] <- sapply(data[, c(1:4, 15:250)], as.character)
+data[, c(1:4, 15:250)] <- sapply(data[, c(1:4, 15:250)], as.numeric)
+
+summary(data)
+hist(data$CN_3p_Del)
+
 
 ################# RNA for GSEA ######################################
 ############http://www.hammerlab.org/################################
