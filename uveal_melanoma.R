2937a3345fe4ad0645316b16daafec164d5725c5
Jun Kang <jun@ip-172-31-1-179.ap-northeast-2.compute.internal>
2016-04-08 05:46:06 +0000

    3p deletion issue

--- uveal_melanoma.R
+++ uveal_melanoma.R
@@ -6,6 +6,7 @@ untar('gdac.broadinstitute.org_UVM.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_e
 
 untar('gdac.broadinstitute.org_UVM-TP.Aggregate_AnalysisFeatures.Level_4.2015082100.0.0.tar.gz', exdir = '.')
 
+untar('gdac.broadinstitute.org_UVM-TP.CopyNumber_Gistic2.Level_4.2015082100.0.0.tar.gz', exdir = '.')
 data <- read.delim('./gdac.broadinstitute.org_UVM-TP.Aggregate_AnalysisFeatures.Level_4.2015082100.0.0/UVM-TP.transferedsamplefeatures.txt')
 
 data[1:5,1:5]
@@ -18,8 +19,11 @@ data[, c(1:4, 15:250)] <- sapply(data[, c(1:4, 15:250)], as.character)
 data[, c(1:4, 15:250)] <- sapply(data[, c(1:4, 15:250)], as.numeric)
 
 summary(data)
-hist(data$CN_3p_Del)
+colnames(data)
 
+boxplot(data$CN_3p_Del ~ as.factor(data$CLUS_CN_cNMF))
+
+########### Cluster CN NMF ##########################################
 
 ################# RNA for GSEA ######################################
 ############http://www.hammerlab.org/################################
