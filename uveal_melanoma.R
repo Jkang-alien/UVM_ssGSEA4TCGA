9c79d333e8009962f4ebe5fa6b3ec7dd555fd7db
Jun Kang <jun@ip-172-31-1-179.ap-northeast-2.compute.internal>
2016-04-22 02:07:09 +0000

    GSEA done

--- uveal_melanoma.R
+++ uveal_melanoma.R
@@ -18,26 +18,57 @@ data <- data.frame(data_gdac[-dim(data_gdac)[1],])
 
 data[, c(1:4, 15:250)] <- sapply(data[, c(1:4, 15:250)], as.character)
 data[, c(1:4, 15:250)] <- sapply(data[, c(1:4, 15:250)], as.numeric)
+data$ID <- rownames(data)
 
 summary(data)
 colnames(data)
 
+ID_class1 <- data$ID[data$CLUS_CN_cNMF == 1]
+ID_class2 <- data$ID[data$CLUS_CN_cNMF == 2]
+ID_class3 <- data$ID[data$CLUS_CN_cNMF == 3]
+
 boxplot(data$CN_3p_Del ~ as.factor(data$CLUS_CN_cNMF))
-data$ID <- rownames(data)
+
 
 ########### Cluster CN NMF ##########################################
 
 ################# RNA for GSEA ######################################
 ############http://www.hammerlab.org/################################
+library(RTCGAToolbox)
 
+getFirehoseDatasets()
+getFirehoseRunningDates(last = NULL)
+readData = getFirehoseData (dataset="UVM", runDate="20151101",forceDownload = TRUE,
+                            Clinic=TRUE, Mutation=FALSE, Methylation=FALSE, RNAseq2_Gene_Norm=TRUE)
 
-rna <- read.delim('./gdac.broadinstitute.org_UVM.Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3.2016012800.0.0/UVM.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt')
-rna <- as.matrix(rna[-1,])
-#row_number_XIST <- grep('XIST', rna$Hybridization.REF)
-dim(rna)
-rna[1,-1]
-length(unlist(strsplit(rna[,1], '\\|'))[seq(2,2*(dim(rna)[1]), 2)])
-rownames(rna) <- unlist(strsplit(rna[,1], '\\|'))[seq(2,2*(dim(rna)[1]), 2)]
-ID <- gsub('.[0-9A-Z]{3}.[0-9A-Z]{3}.[0-9A-Z]{4}.[0-9A-Z]{2}', '', colnames(rna))[-1]
-index_dup <- duplicated(ID)
-sum(index_dup)
+clin = getData(readData, "Clinical")
+mRNA <- getData(readData, 'RNASeq2GeneNorm')
+
+colnames(mRNA) <- gsub('-...-...-....-..', '', colnames(mRNA))
+colnames(mRNA) <- gsub('-', '\\.', colnames(mRNA))
+colnames(mRNA)
+
+data_GSEA <- c()
+data_GSEA <- cbind(mRNA[, colnames(mRNA) %in% ID_class1],
+                   mRNA[, colnames(mRNA) %in% ID_class2],
+                   mRNA[, colnames(mRNA) %in% ID_class3])
+
+GSEA <- data.frame(NAME = rownames(data_GSEA), 
+                   DESCRIPTION = rep('na', dim(data_GSEA)[1]), data_GSEA)
+cls <- c(rep(0, length(ID_class1)),
+         rep(1, length(ID_class2)),
+         rep(2, length(ID_class3)))
+
+write.table(GSEA, file = 'data.txt', append = FALSE, 
+            quote = FALSE, sep = '\t',
+            row.names = FALSE,
+            col.names = TRUE 
+)
+
+write(cls, file = 'cls.cls', append = FALSE, 
+      #quote = FALSE, 
+      sep = '\t',
+      #row.names = FALSE,
+      #col.names = TRUE, 
+      ncolumns = length(cls)
+)
