3f4e6be81f68b367d7252cc5d47bcf1c510851b8
Jun Kang <jun@ip-172-31-1-179.ap-northeast-2.compute.internal>
2016-10-06 06:18:54 +0000

    PCA, clustering

--- GSE39717.R
+++ GSE39717.R
@@ -5,6 +5,12 @@ library(Biobase)
 library(GEOquery)
 library(GSVA)
 library(ssGSEA4TCGA)
+library(ConsensusClusterPlus)
+library(NMF)
+library(Cairo)
+library(survival)
+library(rms)
+
 
 
 data_P1 <- read.csv('Supplementary_Table_S1_Worley_et_al.csv', header = TRUE,
@@ -23,8 +29,6 @@ clin <- merge(data_P1, sample, by.x = 'MM', by.y = 'ID')
 summary(clin)
 summary(sample)
 
-ID_Chr3_disomy <- clin$Accession[clin$Chr3_status_aCGH == 'Disomy']
-ID_Chr3_monosomy <- clin$Accession[clin$Chr3_status_aCGH == 'Monosomy']
 ######## https://www.bioconductor.org/packages/3.3/bioc/vignettes/GEOquery/inst/doc/GEOquery.html#series
 gse<- getGEO(filename = 'GSE39717_family.soft.gz')
 
@@ -35,24 +39,23 @@ head(gsmplatforms)
 gsmlist = Filter(function(gsm) {Meta(gsm)$platform=='GPL6098'},GSMList(gse))
 length(gsmlist)
 
-Table(gsmlist[[2]])[1:5,]
+tab <- Table(gsmlist[[2]])
 Columns(gsmlist[[1]])[1:5,]
 Table(GPLList(gse)[[1]])[2][1:10,]
 
-probesets <- Table(GPLList(gse)[[1]])[2]
+probesets <- Table(GPLList(gse)[[1]])
+
 # make the data matrix from the VALUE columns from each GSM
 # being careful to match the order of the probesets in the platform
 # with those in the GSMs
 
 data.matrix <- do.call('cbind',lapply(gsmlist,function(x) 
 {tab <- Table(x)
-mymatch <- match(probesets$ID,tab$ID_REF)
-return(tab$VALUE)
+mymatch <- match(GPLLIST_table$ID,tab$ID_REF)
+return(tab$VALUE[mymatch])
 }))
 
-mymatch <- match(probesets$ID,tab$ID_REF)
-dim(Table(gsmlist[[1]]))
-data.matrix <- apply(data.matrix,2,function(x) {as.numeric(as.character(x))})
+#data.matrix <- apply(data.matrix,2,function(x) {as.numeric(as.character(x))})
 
 require(Biobase)
 # go through the necessary steps to make a compliant ExpressionSet
@@ -70,29 +73,6 @@ dim(data.matrix)
 
 data.matrix <- data.matrix[complete.cases(data.matrix),]
 
-data_GSEA <- c()
-data_GSEA <- cbind(data.matrix[, colnames(data.matrix) %in% ID_Chr3_monosomy],
-                   data.matrix[, colnames(data.matrix) %in% ID_Chr3_disomy])
-
-sum(colnames(data.matrix) %in% ID_Chr3_monosomy)
-sum(colnames(data.matrix) %in% ID_Chr3_disomy)
-GSEA <- data.frame(NAME = rownames(data.matrix), 
-                   DESCRIPTION = rep('na', dim(data_GSEA)[1]), data_GSEA)
-
-write.table(GSEA, file = 'GSEA_GSE39717.txt', 
-            quote = FALSE, row.names = FALSE, sep = '\t')
-
-cls <- c(rep(0, sum(colnames(data.matrix) %in% ID_Chr3_monosomy)),
-         rep(1, sum(colnames(data.matrix) %in% ID_Chr3_disomy)))
-
-write(cls, file = 'cls_GSE39717.cls', append = FALSE, 
-      #quote = FALSE, 
-      sep = '\t',
-      #row.names = FALSE,
-      #col.names = TRUE, 
-      ncolumns = length(cls)
-)
-
 gs <- gs_gmt('custom.bindea_correct.gmt')
 data <- gsva(data.matrix, gs, method = 'ssgsea')
 data_t <- scale(t(data), center = TRUE, scale = TRUE)
@@ -100,12 +80,15 @@ data_t <- scale(t(data), center = TRUE, scale = TRUE)
 pca <- prcomp(data_t,
               center = FALSE,
               scale = FALSE) 
-predict(pca, 
-        newdata=tail(data[,256:283], 2))
-Chr3[match(clin$Accession, rownames(data_t))] <- clin$Chr.3.status._aCGH
-clin$Chr.3.status._aCGH[is.na(clin$Chr.3.status._aCGH)] <- clin$Chr.3.status.FISH[is.na(clin$Chr.3.status._aCGH)]
 
-Chr3 <- append(Chr3, NA)
+#Chr3[match(clin$Accession, rownames(data_t))] <- clin$Chr.3.status._aCGH
+clin$Chr.3.status._aCGH[is.na(clin$Chr.3.status._aCGH)] <- clin$Chr.3.status.FISH[is.na(clin$Chr.3.status._aCGH)]
+ID_Chr3_disomy <- clin$Accession[clin$Chr.3.status._aCGH == 'Disomy']
+ID_Chr3_monosomy <- clin$Accession[clin$Chr.3.status._aCGH == 'Monosomy']
+Chr3 <- rep(NA, 41)
+Chr3[rownames(data_t) %in% ID_Chr3_disomy] <- 'Disomy'
+Chr3[rownames(data_t) %in% ID_Chr3_monosomy] <- 'Monosomy'
+
 
 library(devtools)
 install_github("ggbiplot", "vqv")
@@ -118,10 +101,40 @@ g <- ggbiplot(pca,
 g <- g + scale_color_discrete(name = '')
 g <- g + theme(legend.direction = 'horizontal', 
                legend.position = 'top')
+
+CairoPDF(file = 'GSE39717_PCA.pdf',
+         width =7.5, height = 7.5, pointsize = 16)
 print(g)
+dev.off()
 
+results_col = ConsensusClusterPlus(data_t,maxK=6,reps=5000,pItem=0.8,pFeature=1,
+                                   title='consensus_col_gse9717',
+                                   clusterAlg="hc",
+                                   innerLinkage = "ward.D2",
+                                   finalLinkage = "ward.D2",
+                                   distance="euclidean",
+                                   plot="pdf")
 
-boxplot(data_t[,23]~Chr3)
+results_row = ConsensusClusterPlus(data,maxK=6,reps=5000,pItem=0.8,pFeature=1,
+                                   title='consensus_row_gse9717',
+                                   clusterAlg="hc",
+                                   innerLinkage = "ward.D2",
+                                   finalLinkage = "ward.D2",
+                                   distance="euclidean",
+                                   plot="pdf")
 
+ann_col <- data.frame(immune_class = factor(results_col[[3]]$consensusClass))
+ann <- data.frame(class = as.factor( results_row[[3]]$consensusClass),
+                  Chr3 = Chr3)
 
+CairoPDF(file = 'GSE39717_clustering.pdf',
+         width =7.5, height = 7.5, pointsize = 16)
+aheatmap(data_t,
+         hclustfun=function(d) hclust(dist(d, method = 'euclidean'), method = "ward.D2"),
+         annRow = ann,
+         annCol = ann_col,
+         Colv = results_col[[3]]$consensusTree,
+         Rowv = results_row[[3]]$consensusTree,
+         labRow = rep('',dim(data_t)[1]))
+dev.off()
               
